0ea3404fcade10a8ea8387e84c2bc87eb74f9776
RGMIllumination <76160050+RGMIllumination@users.noreply.github.com>
2020-12-18 14:41:58 +0100

    Insert Characters

--- Assets/Script/Commands.cs
+++ Assets/Script/Commands.cs
@@ -4,32 +4,48 @@ using UnityEngine;
 
 public class Commands : MonoBehaviour
 {
-
     Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
-        animator = GameObject.FindGameObjectsWithTag(“Player”);
+        animator = GetComponent<Animator>();
+        StartCoroutine(ChangeAnimations());
     }
 
 
     public void Jump()
     {
-     animator.SetBool(“GoJump", true);
-     animator.SetBool(“SayHi”, false);
+        animator.SetBool("GoJump", true);
+        animator.SetBool("SayHi", false);
     }
 
     public void Hello()
     {
-     animator.SetBool(“SayHi”, true);
-     animator.SetBool(“GoJump”, false);
+        animator.SetBool("SayHi", true);
+        animator.SetBool("GoJump", false);
     }
 
     public void Idle()
     {
-     animator.SetBool(“SayHi”, false);
-     animator.SetBool(“GoJump", false);
+        animator.SetBool("SayHi", false);
+        animator.SetBool("GoJump", false);
     }
 
+
+
+
+    public IEnumerator ChangeAnimations()
+    {
+        Idle();
+        yield return new WaitForSeconds(5.0f);
+
+        Hello();
+        yield return new WaitForSeconds(5.0f);
+
+        Jump();
+        yield return new WaitForSeconds(5.0f);
+
+        StartCoroutine(ChangeAnimations());
+    }
 }
